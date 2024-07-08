1b953062ab62026fa617572d1b365e10966ea1ce
Hunter Fuller <hfuller@pixilic.com>
2024-07-07 22:36:26 -0500

    dlroutemap v6: Add Tabulator via GM function
    Delta has added some sort of CORS thing or something that is blocking Tabulator from being loaded externally. We use GM_addElement to load it instead.

--- dlroutemap.user.js
+++ dlroutemap.user.js
@@ -3,12 +3,12 @@
 // @namespace    http://github.com/hfuller/user-js
 // @updateURL    https://github.com/hfuller/user-js/raw/main/dlroutemap.user.js
 // @downloadURL  https://github.com/hfuller/user-js/raw/main/dlroutemap.user.js
-// @version      5
+// @version      6
 // @description  Make the route map not so... scrolly...
 // @author       Hunter Fuller <hfuller@pixilic.com>
 // @match        https://www.delta.com/*/route-map
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=delta.com
-// @grant        none
+// @grant        GM_addElement
 // ==/UserScript==
 
 (function() {
@@ -21,6 +21,17 @@
         }
     }
     document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+
+    console.log("Adding lib");
+    GM_addElement('script', {
+        src: 'https://unpkg.com/tabulator-tables@5.5.0/dist/js/tabulator.min.js',
+        type: 'text/javascript'
+    });
+    GM_addElement('link', {
+        rel: 'stylesheet',
+        href: 'https://unpkg.com/tabulator-tables@5.5.0/dist/css/tabulator.min.css'
+    });
+
     setTimeout(function() {
         for ( let el of document.getElementsByClassName('container-custom') ) {
             console.log("We are going to dink with the container");
@@ -47,6 +58,12 @@
             el.click();
         }
 
+        for ( let el of document.getElementsByClassName("mat-button-wrapper") ) {
+            el.addEventListener('click', function() {
+                document.getElementsByTagName('main')[0].style.zIndex = 999; //just below calendar which is at 1000
+            });
+        }
+
         document.getElementsByClassName('search-button')[0].addEventListener('click', function() {
             document.getElementById("notmap").style.display = "none";
             document.getElementById("map").style.display = "block";
@@ -63,7 +80,7 @@
                 let tabulate = document.getElementsByClassName('title')[1]; //HACK
                 tabulate.innerHTML = "Make Table";
                 tabulate.addEventListener('click', makeFlightTable);
-            }, 1000);
+            }, 1500);
         });
         document.getElementsByClassName('search-button')[0].nextSibling.remove();
 
@@ -77,16 +94,21 @@
         */
     }, 1000);
 
+    /*
     console.log("Adding lib");
     let fileref = document.createElement('script');
+    fileref.id = "hfLib";
     fileref.src = "https://unpkg.com/tabulator-tables@5.5.0/dist/js/tabulator.min.js";
     document.getElementsByTagName("head")[0].appendChild(fileref);
     let fileref2 = document.createElement('link');
     fileref2.rel = "stylesheet";
     fileref2.href = "https://unpkg.com/tabulator-tables@5.5.0/dist/css/tabulator.min.css";
     document.getElementsByTagName("head")[0].appendChild(fileref2);
+    */
+
 
     window.makeFlightTable = function() {
+
         let flights = [];
         for ( let el of document.getElementsByClassName('flight-code') ) {
             let first = true;
@@ -122,7 +144,7 @@
         main.style.top = "0px";
         main.style.height = "100%";
         main.style.background = "white";
-        main.style.zIndex = "1982347198";
+        main.style.zIndex = "931857895797";
         for ( let el of document.getElementsByClassName("zoom-container") ) { el.style.display = "none"; }
         for ( let el of document.getElementsByClassName("legend-box") ) { el.style.display = "none"; }
 
